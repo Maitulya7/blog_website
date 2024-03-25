99598e5b2cc01730f747fd19f4aa8c312d9ebedf
Maitulya Vaghela <vaghelamaitulya85@gmail.com>
2024-03-25 21:07:23 +0530

    user register api

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,16 +1,34 @@
+const User = require("../models/userModel")
+const asyncHandler = require("express-async-handler")
+const bcrypt = require('bcrypt')
 
-const userLogin = (req , res) =>{
+const userLogin = asyncHandler( async (req , res) =>{
     const {username , password } = req.body
     res.json({requestData : {
         username,
         password
     }})
     res.json({message:"you are successfully login"})
-}
+})
 
-const userRegister = (req , res) =>{
-    res.json({message:"you are successfully register"})
-}
+const userRegister = asyncHandler( async (req , res) =>{
+    const {username , password} = req.body
+
+    if(!username || !password){
+        res.status(400);
+        throw new Error("All fields are required")
+    }
+
+    const userAvailable = await User.findOne({email})
+
+    if(userAvailable){
+        res.status(400);
+        throw new Error("User already Available")
+    }
+
+    const hashPassword = await bcrypt.hash(password , 10)
+
+})
 
 module.exports = {
     userLogin ,
