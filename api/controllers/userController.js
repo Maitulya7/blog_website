1f2f3915261068244e30064cbc1cbd815b2f70e8
Maitulya Vaghela <vaghelamaitulya85@gmail.com>
2024-03-25 22:21:13 +0530

    Login Register Completed

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,36 +1,75 @@
 const User = require("../models/userModel")
 const asyncHandler = require("express-async-handler")
 const bcrypt = require('bcrypt')
+const jwt = require("jsonwebtoken")
 
-const userLogin = asyncHandler( async (req , res) =>{
-    const {username , password } = req.body
-    res.json({requestData : {
-        username,
-        password
-    }})
-    res.json({message:"you are successfully login"})
-})
-
-const userRegister = asyncHandler( async (req , res) =>{
-    const {username , password} = req.body
-
-    if(!username || !password){
+const userLogin = asyncHandler(async (req, res) => {
+    const { username, password } = req.body
+    if (!username || !password) {
         res.status(400);
         throw new Error("All fields are required")
     }
 
-    const userAvailable = await User.findOne({email})
+    const user = await User.findOne({ username });
 
-    if(userAvailable){
-        res.status(400);
-        throw new Error("User already Available")
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                id: user.id
+            }
+        },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: "15m" }
+        )
+        res.status(200).json({ accessToken })
+    } else {
+        res.status(401)
+        throw new Error("Username or Password is invalid")
     }
 
-    const hashPassword = await bcrypt.hash(password , 10)
+    res.json({ message: "you are successfully login" })
+})
 
+const userRegister = asyncHandler(async (req, res) => {
+    const { username, password } = req.body
+    if (!username || !password) {
+        res.status(400);
+        throw new Error("All fields are required")
+    }
+
+    const userAvailable = await User.findOne({ username });
+
+    if (userAvailable) {
+        res.status(400);
+        throw new Error("User already registered");
+    }
+
+    //hash password
+    const hashedPassword = await bcrypt.hash(password, 10)
+    console.log("hashedPassword: ", hashedPassword, password)
+
+    try {
+        const user = await User.create({
+            username,
+            password: hashedPassword
+        });
+        console.log(`User created ${user}`);
+        res.status(201).json({ _id: user.id, username: user.username });
+    } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+
+    if (user) {
+        res.status(201).json({ _id: user.id, username: user.username })
+    } else {
+        res.status(400);
+        throw new Error("User data is not valid")
+    }
 })
 
 module.exports = {
-    userLogin ,
+    userLogin,
     userRegister
 }
